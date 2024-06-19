853667d8ff96f295868532a45533cf9d86f6d9d1
b.amirov <amirovbobur52@gmail.com>
2024-06-19 19:57:08 +0500

    added models

--- social_media/settings/base.py
+++ social_media/settings/base.py
@@ -109,7 +109,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
